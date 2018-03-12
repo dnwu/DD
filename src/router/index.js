594623cc266b04801e63370e978b777a29e9767f
Dwan <443881762@qq.com>
2018-03-12 19:48:37 +0800

    框架搭建

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+const Login = () => import('@/components/Login/Login')
+const Index = () => import('@/components/Index/Plan/List')
+
+Vue.use(Router)
+
+let router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index,
+      redirect: '/index/list',
+
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+
+    // if (!auth.loggedIn()) {
+    //   next({
+    //     path: '/login',
+    //     query: { redirect: to.fullPath }
+    //   })
+    // } else {
+    //   next()
+    // }
+  } else {
+    next() // 确保一定要调用 next()
+  }
+})
+export default router
